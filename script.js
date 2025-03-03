b1ca5c0053f34c743f12426c2cd02b8d60a21d75
Rambod Goshtasbi <rpg225@gmail.com>
2025-03-03 00:24:11 -0500

    started the let statements and some of the javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+const { DOWN } = require("phaser");
+
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementByIdr("erase-btn");
+let paintBtn = document.getElementByIdr("paint-btn");
+let widthValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
+
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup"
+    },
+    touch: {
+        down: "touchstart",
+        mobe: "touchmove",
+        up: "touchend",
+    },
+};
+
+let deviceType = "";
+
+let draw = false;
+let erase = false;
+
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
+
+gridButton.addEventListener("click", () => {
+
+});
